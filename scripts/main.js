39a3616ce7839cdae4ca854d650c2cc75ecee2f4
Kayleigh B <jankayleigh@gmail.com>
2017-03-21 20:28:43 -0400

    adding files

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+// Set up the nav scrolling.
+$('nav').onePageNav();
+//menu toggle button
+$('.menu-icon').click(function(){
+  $('nav li').slideToggle();
+
+});
+
+var menu = $('nav li');
+
+//Show nav again when page is resized.
+$(window).resize(function() {
+  var width = $(window).width();
+
+  if (width > 768 && menu.is (':hidden')) {
+    menu.removeAttr('style');
+  }
+})
